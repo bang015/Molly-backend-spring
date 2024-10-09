2c93281f49442453606dd9b9575b47a57ebf8c48
BDH <dh10162@naver.com>
2024-10-10 03:50:37 +0900

    fix: WebSocketConfig 수정

--- src/main/java/com/example/molly/common/config/WebSocketConfig.java
+++ src/main/java/com/example/molly/common/config/WebSocketConfig.java
@@ -1,8 +1,10 @@
 package com.example.molly.common.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -11,6 +13,16 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+  @Bean
+  public ThreadPoolTaskScheduler taskScheduler() {
+    ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+    scheduler.setPoolSize(1);
+    scheduler.setThreadNamePrefix("wss-heartbeat-thread-");
+    scheduler.initialize();
+    return scheduler;
+
+  }
+
   @Override
   public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
     registry.addEndpoint("/ws/chat").setAllowedOriginPatterns("*");
@@ -18,7 +30,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void configureMessageBroker(@NonNull MessageBrokerRegistry config) {
-    config.enableSimpleBroker("/chat", "/user").setHeartbeatValue(new long[] { 10000, 10000 });
+    config.enableSimpleBroker("/chat", "/user").setHeartbeatValue(new long[] { 10000, 10000 })
+        .setTaskScheduler(taskScheduler());
     config.setApplicationDestinationPrefixes("/app");
   }
 }
